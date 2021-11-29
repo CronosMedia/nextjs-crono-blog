046f7ce1b31cbf576192becf25e965a4cfedea34
CronosMedia <cronosdesignro@gmail.com>
2021-11-30 01:41:17 +0200

    Meta description added

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home({ posts }) {
 					name="viewport"
 					content="width=device-width, initial-scale=1.0"
 				/>
+				<meta name="description" content="Technical blog" />
 				<title>Crono Blog</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
